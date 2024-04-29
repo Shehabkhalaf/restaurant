c0ff6374ee45309d6d3d11d6fb3902568e6326b4
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2024-04-29 08:49:03 +0300

    Update migrations reomve stock and redundant data from user.

--- database/migrations/2024_04_29_052015_create_products_table.php
+++ database/migrations/2024_04_29_052015_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->double('initial_price');
             $table->double('final_price');
             $table->text('description');
-            $table->integer('stock');
             $table->string('image');
             $table->timestamps();
         });
